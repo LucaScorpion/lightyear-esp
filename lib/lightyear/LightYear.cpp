312a9c8785aaf21144cdad9a2eddf7d3b17251db
Luca Scalzotto <luca@scalzotto.nl>
2025-11-21 13:21:44 +0100

    Fix mapping

--- lib/lightyear/LightYear.cpp
+++ lib/lightyear/LightYear.cpp
@@ -19,31 +19,32 @@ void LightYear::setPixel(CRGB color) {
 
 void LightYear::setDisplay(uint32_t* colors) {
     int offset = 0;
-    for (uint8_t i = 0; i < NUM_LEDS_1 / 2; i++) {
-        this->leds1[i] = colors[i + offset];
+
+    for (int i = 0; i < NUM_LEDS_1 / 2; i++) {
+        this->leds1[i] = colors[offset + i];
     }
-    offset += NUM_LEDS_1 / 2;
-    for (uint8_t i = NUM_LEDS_1 - 1; i >= NUM_LEDS_1 / 2; i--) {
-        this->leds1[i] = colors[i + offset];
+    for (int i = 0; i < NUM_LEDS_1 / 2; i++) {
+        this->leds1[NUM_LEDS_1 - 1 - i] = colors[offset + NUM_LEDS_1 / 2 + i];
     }
-    offset += NUM_LEDS_1 / 2;
-    for (uint8_t i = 0; i < NUM_LEDS_2 / 2; i++) {
-        this->leds2[i] = colors[i + offset];
+    offset += NUM_LEDS_1;
+
+    for (int i = 0; i < NUM_LEDS_2 / 2; i++) {
+        this->leds2[i] = colors[offset + i];
     }
-    offset += NUM_LEDS_2 / 2;
-    for (uint8_t i = NUM_LEDS_2 - 1; i >= NUM_LEDS_2 / 2; i--) {
-        this->leds2[i] = colors[i + offset];
+    for (int i = 0; i < NUM_LEDS_2 / 2; i++) {
+        this->leds2[NUM_LEDS_2 - 1 - i] = colors[offset + NUM_LEDS_2 / 2 + i];
     }
-    offset += NUM_LEDS_2 / 2;
-    for (uint8_t i = 0; i < NUM_LEDS_3 / 2; i++) {
-        this->leds3[i] = colors[i + offset];
+    offset += NUM_LEDS_2;
+
+    for (int i = 0; i < NUM_LEDS_3 / 2; i++) {
+        this->leds3[i] = colors[offset + i];
     }
-    offset += NUM_LEDS_3 / 2;
-    for (uint8_t i = NUM_LEDS_3 - 1; i >= NUM_LEDS_3 / 2; i--) {
-        this->leds3[i] = colors[i + offset];
+    for (int i = 0; i < NUM_LEDS_3 / 2; i++) {
+        this->leds3[NUM_LEDS_3 - 1 - i] = colors[offset + NUM_LEDS_3 / 2 + i];
     }
-    offset += NUM_LEDS_3 / 2;
-    for (uint8_t i = 0; i < NUM_LEDS_4; i++) {
-        this->leds4[i] = colors[i + offset];
+    offset += NUM_LEDS_3;
+
+    for (int i = 0; i < NUM_LEDS_4; i++) {
+        this->leds4[i] = colors[offset + i];
     }
 }
