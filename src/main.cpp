701c0c27a4608f997457b4c5c2227afa2f5d654f
Luca Scalzotto <luca@scalzotto.nl>
2025-11-21 11:44:31 +0100

    wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include "../lib/lightyear/LightYear.h"
+
+#define WIFI_SSID "infi guests"
+#define WIFI_PASS "sp@ceinv@der"
 
 LightYear *lightYear;
 
 void setup() {
     lightYear = new LightYear();
-    FastLED.setBrightness(255);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    CRGB statusColor = CRGB::Red;
+    while (WiFi.status() != WL_CONNECTED) {
+        statusColor = statusColor == CRGB::Red ? CRGB::Blue : CRGB::Red;
+        lightYear->setPixel(0, statusColor);
+        lightYear->show();
+        delay(500);
+    }
+
+    // Connected
+    lightYear->setPixel(0, CRGB::Green);
+    lightYear->show();
+    delay(1000);
 }
 
 void loop() {
@@ -16,9 +33,6 @@ void loop() {
     lightYear->show();
     delay(500);
     lightYear->setPixel(0, CRGB::Blue);
-    lightYear->show();
-    delay(500);
-    lightYear->setPixel(0, CRGB::Black);
     lightYear->show();
     delay(500);
     lightYear->setPixel(0, CRGB::White);
